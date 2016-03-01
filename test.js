8aa0ec3a24ee6d833e4ca833a9b252c175e8004c
Tomáš Blatný <blatny.tomas@seznam.cz>
2016-03-01 18:44:01 +0100

    Lesson 1: added HTTP server

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log('Hello world! ' + new Date());
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+http.createServer(
+	function (req, res) {
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end('Hello World\n');
+	}
+).listen(port, hostname,
+	function () {
+		console.log('Server running at http://' + hostname + ':' + port);
+	}
+);
